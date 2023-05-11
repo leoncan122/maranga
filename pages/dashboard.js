b6daf00be9968bdfc4d4ff5edd69b180388461c3
leoncan122 <leon.can122@gmail.com>
2023-05-11 16:17:52 +0200

    useUser

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,9 @@
-import {  withPageAuthRequired } from "@auth0/nextjs-auth0";
+import {  withPageAuthRequired,  } from "@auth0/nextjs-auth0";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function dashboard() {
+    const user = useUser()
+    console.log(user)
     return (
         <div>
             Enter
